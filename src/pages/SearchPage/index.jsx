aad757b4a216b0297f38de1180067433e504ecd4
eoekstpal <park.se.mi47@gmail.com>
2024-02-24 20:26:50 +0900

    searchTerm 가져오기

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom'
+import axios from '../../api/axios';
 
 const SearchPage = () => {
+
+    const [searchResults, setSearchResults] = useState([]);
+
+    const useQuery = () => {
+        return new URLSearchParams(useLocation().search);
+    }
+
+    let query = useQuery();
+    const searchTerm = query.get('q');
+
+    useEffect(() => {
+        if (searchTerm) {
+            fetchSearchMovie(searchTerm);
+        }
+    }, [searchTerm])
+
+    const fetchSearchMovie = async (searchTerm) => {
+        try {
+            const response = await axios.get(
+                `/search/multi?include_audlt=false&query=${searchTerm}`
+            )
+            console.log(response)
+            setSearchResults(response.data.results);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
   return (
     <div>SearchPage</div>
   )
